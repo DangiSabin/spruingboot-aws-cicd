eefec1e5cbeb968c01e63de080e8ec23ecd8e78c
Sabin <sabin4.dangi@gmail.com>
2024-07-20 13:02:04 -0500

    SpringBoot app with aws cicd

---
+++ src/main/java/com/learning/dto/Course.java
@@ -0,0 +1,62 @@
+package com.learning.dto;
+
+import java.util.Objects;
+
+public class Course {
+
+    private int id;
+    private String name;
+    private double price;
+    
+	public Course() {
+	}
+	
+	public Course(int id, String name, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		return id == other.id && Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price);
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}  
+}
